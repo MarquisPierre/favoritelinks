93c350823c954e99f0cbc794e480211869efa553
MarquisPierre <marquispierre27@gmail.com>
2021-12-29 14:32:19 -0500

    Pushing link website components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,66 @@
 import React, {Component} from 'react';
 
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        
-        /*
-            TODO - set initial state for link name and URL 
 
-        */
+    constructor(props) {
+
+        super(props); 
+
+        this.state = {
+
+            linkName: '',
+
+            url: ''
+
+        }
+
     }
 
     handleChange = event => {
-        /*
-            TODO - Logic for changing state based on form changes
-        */
+
+        if(event.target.id === "link")
+
+            this.setState({ linkName: event.target.value}); 
+        else
+
+            this.setState({ url: event.target.value});         
+
     }
 
-    onFormSubmit = (event) => {
-        // to prevent page reload on form submit
+    onFormSubmit = event => { 
+
         event.preventDefault();
-        
-        /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
+        let newFavLink = {linkName: this.state.linkName, url: this.state.url};
+
+        this.props.addNew(newFavLink);
+        this.setState({ linkName: '', url: ''}); 
 
     }
 
     render() {
 
         return(
+
             <form>
-                {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+
+             
+
+                <label htmlFor="link">Link Name : </label>
+
+                <input type="text" id="link" value={this.state.linkName} onChange={(event) =>this.handleChange(event)}/>      
+
+                <label htmlFor="url">URL : </label>
+
+                <input type="text" id="url" value={this.state.url} onChange={(event) =>this.handleChange(event)}/>
+
+                <button type="submit" onClick={(e)=>this.onFormSubmit(e)}>Submit</button>                
+
             </form>
+
         )
-    
+
     }
+
 }
 
 export default Form;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,63 @@
 import React from 'react';
 
-const TableHeader = () => { 
-    // boilerplate table header functional component
+
+const TableHeader = () => {
     return (
         <thead>
             <tr>
                 <th>Name</th>
+
                 <th>URL</th>
+
                 <th>Remove</th>
+
             </tr>
+
         </thead>
+
     );
+
 }
 
-const TableBody = props => { 
-    // boilerplate table body functional component 
-    // we use Array.map to create table rows from LinkData passed via props
+const TableBody = props => {
     const rows = props.linkData.map((row, index) => {
+
         return (
+
             <tr key={index}>
-                <td>{row.name}</td>
-                <td><a href={row.URL}>{row.URL}</a></td>
+
+                <td>{row.linkName}</td>
+                <td><a href={row.url}>{row.url}</a></td>
+
                 <td><button onClick={() => props.removeLink(index)}>Delete</button></td>
+
             </tr>
+
         );
+
     });
 
     return <tbody>{rows}</tbody>;
+
 }
 
 const Table = (props) => {
-//    const { linkData, removeLink } = props;
+
+ const {linkData, removelink} = props;
+
     {/*TODO - return <table> component, TableHeader and TableBody  */}
+
+    return (
+
+        <div>
+
+            {TableHeader()}
+
+            {TableBody(props)}
+
+        </div>
+
+    )
 
 }
 
